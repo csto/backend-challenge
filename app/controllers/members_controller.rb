2e705b80866f779fe9c62821af1451f2925b956b
Csto <coreystout@gmail.com>
2021-02-06 17:51:32 -0500

    Add ability to create, list and show members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,9 +3,29 @@ class MembersController < ApplicationController
     @member = Member.new
   end
 
-  def index
+  def create
+    @member = Member.new(permitted_params)
+
+    if @member.save
+      # ShortLinkService.process(@member)
+      # HeadingsService.process(@member)
+      redirect_to @member
+    else
+      render :new
+    end
   end
 
   def show
+    @member = Member.find(params[:id])
+  end
+
+  def index
+    @members = Member.page(params[:page])
+  end
+
+  private
+
+  def permitted_params
+    params.require(:member).permit(:name, :website_url)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def error_for(model, field)
+    return nil if model.errors.messages[field].blank?
+
+    message = "#{field.to_s.titleize} #{model.errors.messages[field].first}"
+    content_tag :div, message, class: 'error'
+  end
 end
