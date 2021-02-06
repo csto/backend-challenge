8c977bb6fc0a0effbb433d9ce9dfdbe6fcbea401
Csto <coreystout@gmail.com>
2021-02-06 17:30:46 -0500

    Add members controller and materialize for styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+require('./materialize.js')
 
 Rails.start()
 Turbolinks.start()
---
+++ spec/requests/members_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Members", type: :request do
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/members/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/members/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/members/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
